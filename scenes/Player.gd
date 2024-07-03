2aec114a6db529b31f8ccb9cd3596871070f4645
Hellrik321 <ph.lo3321@gmail.com>
2024-07-03 18:14:01 -0300

    separando nodes

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@onready var camera = $Camera3D
+
 @export_category("Player Settings")
 @export var speed := 5.0
 @export var jump_velocity := 4.5
@@ -14,12 +16,12 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var vertical_rotation := 0.0
 
 
-func _input(event):
+func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 		vertical_rotation -= event.relative.y * mouse_sensitivity
 		vertical_rotation = clamp(vertical_rotation, -vertical_rotation_limit, vertical_rotation_limit)
-		$head/Camera3D.rotation_degrees.x = vertical_rotation
+		camera.rotation_degrees.x = vertical_rotation
 		
 	_notification(event)
 		
@@ -33,7 +35,7 @@ func _notification(what):
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	var vertical_rotation = $head/Camera3D.rotation_degrees.x
+	var vertical_rotation = camera
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -41,12 +43,12 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("accept") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
